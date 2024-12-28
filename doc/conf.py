3821c677d56ca506416e950fa5560ca3f7af5d54
ssskiv <63557178+ssskiv@users.noreply.github.com>
2024-12-28 16:36:51 +0300

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,13 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = [
+   'myst_parser',
+   'sphinx.ext.duration',
+   'sphinx.ext.doctest',
+   'sphinx.ext.autodoc',
+   'sphinx.ext.autosummary',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
