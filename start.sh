933adc2ac865fbf8351f307a272b15ad6bfc710d
ssskiv <63557178+ssskiv@users.noreply.github.com>
2025-02-18 13:02:04 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #make all FLAVOR="devel" ROBOT_PANEL_PORT=8008 VS_PORT=31415 WEBOTS_STREAM_PORT=1234
 #docker exec -it ulstu-devel sudo sed -i 's/\r$//g' /ulstu/.bashrc
-docker start ulstu-devel
+docker start ulstu-test
 make start-code-server
-docker exec -it ulstu-devel bash
+docker exec -it ulstu-test bash
