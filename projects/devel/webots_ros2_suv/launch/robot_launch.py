555a84b2d9e381570f8fa45ceff7b9819f22cd26
Ivan Ioshpa <ivanioshpa@gmail.com>
2025-02-19 21:09:54 +0300

    drive paraams tuned

--- projects/devel/webots_ros2_suv/launch/robot_launch.py
+++ projects/devel/webots_ros2_suv/launch/robot_launch.py
@@ -370,7 +370,7 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'world',
-            default_value='robocross_gazelle.wbt',
+            default_value='hard_test.wbt',
             description='Robocross simulation world'
         ),
         webots,
