abbfe1a4cc84d3983e0073b809bf8b83bde212f0
Ivan Ioshpa <ivanioshpa@gmail.com>
2025-02-20 09:47:57 +0000

    fixed ros2_ws

---
+++ ros2_ws/nav_launch.sh
@@ -0,0 +1,1 @@
+ros2 launch nav2_bringup navigation_launch.py params_file:=src/webots_ros2_suv/config/nav2_params.yaml 
---
+++ ros2_ws/run.sh
@@ -0,0 +1,3 @@
+source install/setup.bash
+colcon build
+ros2 launch webots_ros2_suv robot_launch.py
